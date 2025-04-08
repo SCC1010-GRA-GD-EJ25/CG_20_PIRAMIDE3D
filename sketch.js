ed325b0aa11df21745287a92d1a5545ba81ec54d
CarlosGarciaOb <127284600+CMgarciaOB@users.noreply.github.com>
2025-04-08 22:27:44 +0000

    v2.0

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function draw() {
   // put drawing code here
   background(255)
   rotateX(frameCount*0.01)
-  rotateY(frameCount*0.01)
+
  piramide(150)
  fill(255,0,0,0)
  translate(300,0,0)
@@ -24,15 +24,15 @@ function draw() {
 
 function piramide(t){
 textureMode(NORMAL)
-  //base
-  
+ 
+//base
   texture(foto)
   beginShape();
-  vertex(-t,-t,-t);
-  vertex(t,-t,-t);
-  vertex(t,t,-t);
-  vertex(-t,t,-t);
-  vertex(-t,-t,-t);
+  vertex(-t,-t,-t,0,0);
+  vertex(t,-t,-t,1,0);
+  vertex(t,t,-t,1,1);
+  vertex(-t,t,-t,0,1);
+  vertex(-t,-t,-t,0,0);
   endShape()
   
   //cara 1
@@ -47,28 +47,28 @@ textureMode(NORMAL)
   //cara 2
   texture(foto)
   beginShape()
-  vertex(0,0,t)
-  vertex(t,-t,-t)
-  vertex(t,t,-t)
-  vertex(0,0,t)
+  vertex(0,0,t,0.5,0)
+  vertex(t,-t,-t,1,1)
+  vertex(t,t,-t,0,1)
+  vertex(0,0,t,0.5,0)
   endShape()
 
   //cara 3
   texture(foto)
   beginShape()
-  vertex(0,0,t)
-  vertex(t,t,-t)
-  vertex(-t,t,-t)
-  vertex(0,0,t)
+  vertex(0,0,t,0.5,0)
+  vertex(t,t,-t,1,1)
+  vertex(-t,t,-t,0,1)
+  vertex(0,0,t,0.5,0)
   endShape()
 
   //cara 4
   texture(foto)
   beginShape()
-  vertex(0,0,t)
-  vertex(-t,t,-t)
-  vertex(-t,-t,-t)
-  vertex(0,0,t)
+  vertex(0,0,t,0.5,0)
+  vertex(-t,t,-t,1,1)
+  vertex(-t,-t,-t,0,1)
+  vertex(0,0,t,0.5,0)
   endShape()
 }
 
